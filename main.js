cc769de7b97a6c6e1f3de9a8e78d30e682f0c7c6
Muslima Mavlonova <muslimamavlonova5>
2023-03-04 22:59:35 +0500

    my first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var userName = "Муслима";
+var word = prompt("Укажите" + " своё " + " имя" );
+console.log(" Меня" + " зовут " + userName);
+var myAge = 15;
+var word = prompt("Укажите" + " свой " + "возраст" );
+console.log("Мне " + myAge);
+
+
+var word = prompt( "Пример1:" + " 12 + 4 = ?" );
+alert(word);
+var word = prompt( "Пример2:" + " 14 - 8 = ?" );
+alert(word);
+var word = prompt( "Пример3:" + " 27 / 3 = ?" );
+alert(word);
+var word = prompt( "Пример4:" + " 8 * 4 = ?"  ); 
+alert(word);
+var word = prompt( "Пример5:" + " 12 % 2 = ?" );
+alert(word);
+var word = alert( "Откройте" +  " консоль" + " для " + "просмотра" );
+
+
+
+var x = +prompt("введите первое число");
+var y = +prompt("введите второе число");
+var z = +prompt("введите третье число");
+var res = (x+y+z) / 7;
+alert( "Среднее арифмитическое число =" + res);
